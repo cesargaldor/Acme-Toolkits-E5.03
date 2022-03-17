0c047b0e4d7433c47a2bd4d7aff20833a35361af
josrojrom1 <josrojrom1@alum.us.es>
2022-03-17 11:14:28 +0100

    Arreglado errores en entidad announcement. Form y list funcionando aunque este último no muestra todos los atributos.

--- src/main/java/acme/entities/announcements/Announcement.java
+++ src/main/java/acme/entities/announcements/Announcement.java
@@ -15,23 +15,25 @@ package acme.entities.announcements;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
 
 import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.URL;
 
 import acme.framework.entities.AbstractEntity;
+import acme.framework.roles.Administrator;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class Announcement extends AbstractEntity {// Serialisation identifier -----------------------------------------------
+public class Announcement extends AbstractEntity {
+	
+	// Serialisation identifier -----------------------------------------------
  
 	protected static final long	serialVersionUID	= 1L;
 
@@ -45,8 +47,6 @@ public class Announcement extends AbstractEntity {// Serialisation identifier --
 	@Length(min=1, max=101)
 	protected String			title;
 
-
-
 	@NotBlank
 	@Length(min=1, max=256)
 	protected String			body;
@@ -59,5 +59,7 @@ public class Announcement extends AbstractEntity {// Serialisation identifier --
 
 	// Relationships ----------------------------------------------------------
 
+	@ManyToOne
+	Administrator administrator;
 
 }
