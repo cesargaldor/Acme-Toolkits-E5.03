a47ac436da3dddd64ab98203a3f08d4dba8f69d2
josrojrom1 <josrojrom1@alum.us.es>
2022-03-19 19:22:01 +0100

    Añadido modelado y vista junto con algunas queries del Dashboard de Patron

--- src/main/java/acme/features/patron/dashboard/PatronDashboardController.java
+++ src/main/java/acme/features/patron/dashboard/PatronDashboardController.java
@@ -12,6 +12,9 @@
 
 package acme.features.patron.dashboard;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -23,17 +26,17 @@ import acme.roles.Patron;
 @RequestMapping("/patron/dashboard/")
 public class PatronDashboardController extends AbstractController<Patron, Dashboard> {
 
-//	// Internal state ---------------------------------------------------------
-//
-//	@Autowired
-//	protected PatronDashboardShowService showService;
-//
-//	// Constructors -----------------------------------------------------------
-//
-//
-//	@PostConstruct
-//	protected void initialise() {
-//		super.addCommand("show", this.showService);
-//	}
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected PatronDashboardShowService showService;
+
+	// Constructors -----------------------------------------------------------
+
+
+	@PostConstruct
+	protected void initialise() {
+		super.addCommand("show", this.showService);
+	}
 
 }
