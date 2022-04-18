03005ee9ee5d71ad77cdf3faa5a219f094746952
César <cesgaldor@alum.us.es>
2022-04-18 18:30:15 +0200

    Arreglado pequeños fallos en el sample data de algunas entidades

--- src/main/java/acme/entities/quantity/Quantity.java
+++ src/main/java/acme/entities/quantity/Quantity.java
@@ -1,32 +1,38 @@
-
 package acme.entities.quantity;
 
-import java.awt.Toolkit;
-
+import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 import acme.entities.items.Item;
+import acme.entities.toolKits.ToolKit;
 import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
 
-public class Quantity extends AbstractEntity {
-
-	private static final long	serialVersionUID	= 1L;
+@Entity
+@Getter
+@Setter
+public class Quantity extends AbstractEntity{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	@Min(1)
-	protected int					number;
+	protected int number;
 	
-	// Relacion con items
 	@NotNull
 	@Valid
-	@ManyToOne(optional = false)
-	protected Item					item;
+	@ManyToOne(optional=false)
+	protected Item item;
+	
 
-	// Relacion con toolkit
 	@NotNull
 	@Valid
-	@ManyToOne(optional = false)
-	protected Toolkit				toolkit;
+	@ManyToOne(optional=false)
+	protected ToolKit toolkit;
 }
