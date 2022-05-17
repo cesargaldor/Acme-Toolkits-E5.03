6c013b65cea0c80b5f0fdb7f438f614e3179c4ae
César <cesgaldor@alum.us.es>
2022-05-17 18:55:10 +0200

    Arreglados los 3 tests de errores

--- src/test/java/acme/testing/any/userAccount/AnyUserAccountCreate.java
+++ src/test/java/acme/testing/any/userAccount/AnyUserAccountCreate.java
@@ -11,7 +11,7 @@ public class AnyUserAccountCreate extends TestHarness {
 
 	@ParameterizedTest
 	@CsvFileSource(resources = "/any/user-account/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
-	@Order(10)
+	@Order(1)
 	public void createUserPositive(final int recordIndex, final String username, final String password, final String confirmation, final String name, final String surname, final String email, final String phone, final String accept) {
 
 		super.signUp(username, password, name, surname, email, phone);
@@ -24,17 +24,26 @@ public class AnyUserAccountCreate extends TestHarness {
 
 	@ParameterizedTest
 	@CsvFileSource(resources = "/any/user-account/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
-	@Order(10)
+	@Order(12)
 	public void createUserNegative(final int recordIndex, final String username, final String password, final String confirmation, final String name, final String surname, final String email, final String phone, final String accept) {
 
-		super.signUp(username, password, name, surname, email, phone);
+		super.clickOnMenu("Sign up");
+		super.fillInputBoxIn("username", username);
+		super.fillInputBoxIn("password", password);
+		super.fillInputBoxIn("confirmation", password);
+		super.fillInputBoxIn("identity.name", name);
+		super.fillInputBoxIn("identity.surname", surname);
+		super.fillInputBoxIn("identity.email", email);
+		super.fillInputBoxIn("identity.phone", phone);
+		super.fillInputBoxIn("accept", accept);
+		super.clickOnSubmit("Sign up");
 
 		super.checkErrorsExist();
 	}
 
 	@ParameterizedTest
 	@CsvFileSource(resources = "/any/user-account/become-patron-success.csv", encoding = "utf-8", numLinesToSkip = 1)
-	@Order(10)
+	@Order(3)
 	public void becomePatronSuccess(final int recordIndex, final String username, final String password, final String company, final String statement, final String link) {
 
 		super.signIn(username, password);
@@ -54,9 +63,10 @@ public class AnyUserAccountCreate extends TestHarness {
 
 	@ParameterizedTest
 	@CsvFileSource(resources = "/any/user-account/become-patron-error.csv", encoding = "utf-8", numLinesToSkip = 1)
-	@Order(10)
-	public void becomePatronError(final int recordIndex, final String username, final String password, final String company, final String statement, final String link) {
+	@Order(4)
+	public void becomePatronError(final int recordIndex, final String username, final String password, final String confirmation, final String name, final String surname, final String email, final String phone, final String accept, final String company, final String statement, final String link) {
 
+		super.signUp(username, password, name, surname, email, phone);
 		super.signIn(username, password);
 		super.clickOnMenu("Account", "Become a patron");
 
@@ -74,7 +84,7 @@ public class AnyUserAccountCreate extends TestHarness {
 
 	@ParameterizedTest
 	@CsvFileSource(resources = "/any/user-account/become-inventor-success.csv", encoding = "utf-8", numLinesToSkip = 1)
-	@Order(10)
+	@Order(5)
 	public void becomeInventorSuccess(final int recordIndex, final String username, final String password, final String company, final String statement, final String link) {
 
 		super.signIn(username, password);
@@ -94,9 +104,10 @@ public class AnyUserAccountCreate extends TestHarness {
 
 	@ParameterizedTest
 	@CsvFileSource(resources = "/any/user-account/become-inventor-error.csv", encoding = "utf-8", numLinesToSkip = 1)
-	@Order(10)
-	public void becomeInventorError(final int recordIndex, final String username, final String password, final String company, final String statement, final String link) {
+	@Order(6)
+	public void becomeInventorError(final int recordIndex, final String username, final String password, final String confirmation, final String name, final String surname, final String email, final String phone, final String accept, final String company, final String statement, final String link) {
 
+		super.signUp(username, password, name, surname, email, phone);
 		super.signIn(username, password);
 		super.clickOnMenu("Account", "Become a inventor");
 
