6468a7040c9a7df3ce37a129186519aeb54916d3
César <cesgaldor@alum.us.es>
2022-05-22 17:06:44 +0200

    Cambios en los tests y algún arreglo

--- src/test/java/acme/testing/inventor/patronage/InventorPatronageListTest.java
+++ src/test/java/acme/testing/inventor/patronage/InventorPatronageListTest.java
@@ -1,3 +1,4 @@
+
 package acme.testing.inventor.patronage;
 
 import org.junit.jupiter.api.Order;
@@ -6,18 +7,15 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 
 import acme.testing.TestHarness;
 
-public class InventorPatronageListTest extends TestHarness{
-	
-	
-	
-	
+public class InventorPatronageListTest extends TestHarness {
+
 	@ParameterizedTest
 	@CsvFileSource(resources = "/inventor/patronage/list.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
 	public void ListPositiveTest(final int recordIndex, final String status, final String code, final String legalStuff, final String budget, final String moment, final String optionalLink) {
-		super.signIn("inventor1","inventor1");
+		super.signIn("inventor1", "inventor1");
 		super.clickOnMenu("Inventor", "List my patronages");
-		
+		super.sortListing(0, "asc");
 		super.checkColumnHasValue(recordIndex, 0, status);
 		super.checkColumnHasValue(recordIndex, 1, code);
 		super.checkColumnHasValue(recordIndex, 2, legalStuff);
@@ -25,8 +23,7 @@ public class InventorPatronageListTest extends TestHarness{
 		super.checkColumnHasValue(recordIndex, 4, moment);
 		super.checkColumnHasValue(recordIndex, 5, optionalLink);
 		super.signOut();
-		
-		
+
 	}
 
 }
--- src/test/java/acme/testing/patron/patronage/PatronPatronageCreateTest.java
+++ src/test/java/acme/testing/patron/patronage/PatronPatronageCreateTest.java
@@ -1,19 +1,23 @@
+
 package acme.testing.patron.patronage;
 
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.openqa.selenium.By;
 
+import acme.framework.testing.BrowserDriver;
 import acme.testing.TestHarness;
 
-public class PatronPatronageCreateTest extends TestHarness{
-	
-	
+public class PatronPatronageCreateTest extends TestHarness {
+
 	@ParameterizedTest
 	@CsvFileSource(resources = "/patron/patronage/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
-	public void positiveTest(final int recordIndex, final String status, final String code, final String legalStuff, final String budget, final String moment, final String optionalLink, final String username) {
-		
+	public void positiveTest(final int recordIndex, final String status, final String code, final String legalStuff, final String budget, final String moment, final String optionalLink, final String inventorId) {
+
+		final BrowserDriver driver = super.getDriver();
+
 		super.signIn("patron1", "patron1");
 		super.clickOnMenu("Patron", "List all patronages");
 		super.clickOnButton("Create patronage");
@@ -24,13 +28,11 @@ public class PatronPatronageCreateTest extends TestHarness{
 		super.fillInputBoxIn("budget", budget);
 		super.fillInputBoxIn("moment", moment);
 		super.fillInputBoxIn("optionalLink", optionalLink);
-		super.fillInputBoxIn("username", username);
-
+		super.fillInputBoxIn("inventorId", inventorId);
+		driver.locateOne(By.xpath("//*[@id=\"inventorId\"]/option[" + inventorId + "]")).click();
 
 		super.clickOnSubmit("Create patronage");
-
 		super.signOut();
-		
 	}
 
 }
